4f51279240bccd8f8cd27bee4d81db269575029e
Prajwal-codilar <prajwal.j@codilar.com>
2022-07-21 15:38:14 +0530

    get Customer name in PDP page

---
+++ Prajwal/Assignment6/CustomerData/CustomSection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Prajwal\Assignment6\CustomerData;
+
+use Magento\Customer\CustomerData\SectionSourceInterface;
+use Magento\Customer\Model\Session;
+
+class CustomSection implements SectionSourceInterface
+{
+    /**
+     * @var Session
+     */
+    protected Session $customerSession;
+
+    /**
+     * CustomSection constructor.
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Session $customerSession
+    ) {
+        $this->customerSession=$customerSession;
+    }
+
+    /**
+     * Return the Customer details
+     *
+     * @return array|string[]
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getSectionData()
+    {
+        return [
+            'customerdetail' =>date('H:i:s') .$this->customerSession->getCustomer()->getName(),
+        ];
+    }
+}
